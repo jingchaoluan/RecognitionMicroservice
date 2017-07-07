38c39084c55cc3dfc4f101afd210570da1ba0657
jingchaoluan <jingchaoluan@gmail.com>
2017-07-06 20:06:42 -0400

    Deleted parameters that cannot be set by an service call

--- api/serializers.py
+++ api/serializers.py
@@ -5,6 +5,4 @@ from api.models import Parameters
 class ParameterSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Parameters
-		fields = ('id', 'nolineest', 'height', 'pad', 'nonormalize', 'llocs', 
-			'alocs', 'probabilities', 'estrate', 'estconf', 'compare', 'context', 
-			'quiet', 'nocheck', 'parallel')
+		fields = ('id', 'height', 'pad', 'nonormalize', 'llocs', 'probabilities')
